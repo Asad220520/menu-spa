aaf2e3059db2f412f0b24f459c7098fcb3526e33
Marlen <ttv90407@gmail.com>
2025-09-29 18:44:05 +0600

    menuDetaile

--- src/pages/menu/MenuDetaile.jsx
+++ src/pages/menu/MenuDetaile.jsx
@@ -1,3 +1,158 @@
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import styles from "./MenuDetaile.module.scss";
+// import { useParams, useLocation } from "react-router-dom";
+
+// const MenuDetaile = () => {
+//   const { id } = useParams();
+//   const location = useLocation();
+//   const productId = location.state?.id || id;
+
+//   const [categories, setCategories] = useState([]);
+//   const [activeCategory, setActiveCategory] = useState(null);
+//   const [product, setProduct] = useState(null);
+//   const [relatedProducts, setRelatedProducts] = useState([]);
+
+//   // Категорияларды алуу
+//   useEffect(() => {
+//     const fetchCategories = async () => {
+//       try {
+//         const res = await axios.get("http://13.60.41.77/ru/category/");
+//         setCategories(res.data);
+//       } catch (err) {
+//         console.error("Категорияларды алуу ката чыкты:", err);
+//       }
+//     };
+//     fetchCategories();
+//   }, []);
+
+//   // Продукттун деталдары жана окшош продуктылар
+//   useEffect(() => {
+//     if (!productId) return;
+
+//     const fetchProduct = async () => {
+//       try {
+//         const res = await axios.get(
+//           `http://13.60.41.77/ru/product/${productId}/`
+//         );
+//         setProduct(res.data);
+
+//         if (res.data.category) {
+//           const related = await axios.get(
+//             `http://13.60.41.77/ru/product/?category=${res.data.category}`
+//           );
+//           setRelatedProducts(
+//             related.data.filter((p) => p.id !== res.data.id).slice(0, 5)
+//           );
+//         }
+//       } catch (err) {
+//         console.error("Продукт деталдары ката чыкты:", err);
+//       }
+//     };
+
+//     fetchProduct();
+//   }, [productId]);
+
+//   if (!product) return <p>Loading...</p>;
+
+//   console.log(product);
+
+//   return (
+//     <div className={styles.container}>
+//       {/* Сол жактагы категориялар */}
+//       <div className={styles.categories}>
+//         {categories.map((cat) => (
+//           <div
+//             key={cat.id}
+//             className={`${styles.categoryItem} ${
+//               activeCategory === cat.id ? styles.active : ""
+//             }`}
+//             onClick={() => setActiveCategory(cat.id)}
+//           >
+//             {cat.category_name}
+//           </div>
+//         ))}
+//       </div>
+
+//       {/* Продукт деталдары */}
+//       <div className={styles.menuContainer}>
+//         <div className={styles.productBlocking}>
+//           <img
+//             src={product.product_image}
+//             alt={product.product_name}
+//             className={styles.product_image}
+//           />
+//           <div className={styles.productRow}>
+//             <h3 className={styles.product_name}>{product.product_name}</h3>
+
+//             <p className={styles.product_price}>{product.price}$</p>
+//           </div>
+//           {product.product_ingradient?.length > 0 && (
+//             <p className={styles.product_ingradient}>
+//               {product.product_ingradient
+//                 .map((i) => i.ingradient_name)
+//                 .join(", ")}
+//             </p>
+//           )}
+//         </div>
+
+//         <div className={styles.list_ex_dr}>
+//           {/* Extras */}
+//           {product.product_extras?.length > 0 && (
+//             <div className={styles.extras}>
+//               <b className={styles.title}>Extras:</b>
+//               <ul className={styles.list}>
+//                 {product.product_extras.map((ex) => (
+//                   <li key={ex.id} className={styles.item}>
+//                     <span>{ex.extras_name}</span>
+//                     <span>{ex.price} $</span>
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
+//           )}
+//           {/* Drinks */}
+//           {product.product_drinks?.length > 0 && (
+//             <div className={styles.drinks}>
+//               <b className={styles.title}>Drinks:</b>
+//               <ul className={styles.list}>
+//                 {product.product_drinks.map((dr) => (
+//                   <li key={dr.id} className={styles.item}>
+//                     <span>{dr.drink_name}</span>
+//                     <span>{dr.price} $</span>
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+
+//       <div className={styles.productBlocks}>
+//         {relatedProducts.map((p) => (
+//           <div key={p.id} className={styles.productCard}>
+//             <img
+//               src={p.product_image}
+//               alt={p.product_name}
+//               className={styles.product_image}
+//             />
+//             <h3>{p.product_name}</h3>
+//             <p className={styles.product_price}>{p.price} $</p>
+//             {p.product_ingradient?.length > 0 && (
+//               <p className={styles.product_ingradient}>
+//                 {p.product_ingradient.map((i) => i.ingradient_name).join(", ")}
+//               </p>
+//             )}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MenuDetaile;
+
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
